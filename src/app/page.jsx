f8152eb699368edf80ad9b70ddd02abadbbba9c4
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-12-27 23:59:14 +0700

    feat: styled home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "@/../public/hero.png";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Halooo saat ini saya gabut.</h1>
+        <p className={styles.desc}>Pengen Tian Mimi deh</p>
+        <button className={styles.button}>See our works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="Hero Image" className={styles.img} />
+      </div>
+    </div>
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>Copyright Rafpo 2025. All rights reserved</div>
-      <div></div>
+      <div className={styles.socials}>
+        <Image
+          className={styles.icon}
+          src="/1.png"
+          alt="Full of Joy FB"
+          width={15}
+          height={15}
+        />
+        <Image
+          className={styles.icon}
+          src="/2.png"
+          alt="Full of Joy IG"
+          width={15}
+          height={15}
+        />
+        <Image
+          className={styles.icon}
+          src="/3.png"
+          alt="Full of Joy TW"
+          width={15}
+          height={15}
+        />
+        <Image
+          className={styles.icon}
+          src="/4.png"
+          alt="Full of Joy YT"
+          width={15}
+          height={15}
+        />
+      </div>
     </div>
   );
 };
