242dfc1e8c7134bad9a3573b8f8653f3bd8921db
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-12-30 23:21:55 +0700

    feat: contact page + animations

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/Button/Button";
 
 const Contact = () => {
-  return <div className={styles.contact}>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let&apos;s Keep in touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            className={styles.input}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            className={styles.input}
+          />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            cols={30}
+            rows={10}
+          />
+          <Button url="#" text={"Send"} />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
