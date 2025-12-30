52350aca2457046197c9d766e97e0f7b80ba0ad8
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-12-31 00:44:00 +0700

    feat: blog page created

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,8 +1,63 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 const BlogPost = () => {
-  return <div className={styles.container}>BlogPost</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.desc}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis porro nobis dolore. Repudiandae earum ducimus fuga,
+            minima ullam quia in. Enim cum et rerum maxime doloribus omnis,
+            voluptate deserunt aperiam?
+          </p>
+          <div className={styles.author}>
+            <Image
+              alt=""
+              src={
+                "https://images.pexels.com/photos/32329293/pexels-photo-32329293.jpeg"
+              }
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>John Doe</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src={
+              "https://images.pexels.com/photos/13151566/pexels-photo-13151566.jpeg"
+            }
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero a,
+          exercitationem molestias consequatur porro ullam rerum? Velit minus
+          sed vel, dolores labore possimus, eos eaque iusto voluptate ea
+          temporibus esse cumque? Expedita accusantium exercitationem quod
+          omnis, quia suscipit repellendus fugiat facere quae eligendi dicta
+          consectetur dolorem tempora perferendis eaque ipsum nostrum minus.
+          Earum facilis at aperiam qui quisquam totam minus sed magni aut. Esse
+          accusamus dicta exercitationem provident tempora, alias reprehenderit
+          officiis nulla perspiciatis aliquam eligendi voluptatum dolores ex,
+          nihil doloribus nisi quaerat? Rem a eligendi consequuntur cumque
+          possimus ipsa vitae id nostrum alias, rerum at non perspiciatis sequi
+          labore?
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,64 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Blog = () => {
-  return <div className={styles.container}>Blog</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link href={"/blog/testId"} className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            alt="image contoh"
+            src={
+              "https://images.pexels.com/photos/35051828/pexels-photo-35051828.jpeg"
+            }
+            width={400}
+            height={250}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>desc</p>
+        </div>
+      </Link>
+      <Link href={"/blog/testId"} className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            alt="image contoh"
+            src={
+              "https://images.pexels.com/photos/35051828/pexels-photo-35051828.jpeg"
+            }
+            width={400}
+            height={250}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>desc</p>
+        </div>
+      </Link>
+      <Link href={"/blog/testId"} className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            alt="image contoh"
+            src={
+              "https://images.pexels.com/photos/35051828/pexels-photo-35051828.jpeg"
+            }
+            width={400}
+            height={250}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>desc</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
