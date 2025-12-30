fb3e4b0107bb5e5163655d1871fa634f08ab6ead
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-12-31 00:20:44 +0700

    feat: portfolio page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,52 @@
+"use client";
+
 import React from "react";
 import styles from "./page.module.css";
+import { useParams } from "next/navigation";
+import Button from "@/components/Button/Button";
+import Image from "next/image";
 
 const Category = () => {
-  return <div className={styles.container}>Category</div>;
+  const params = useParams();
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>desc</p>
+          <Button text={"See More"} url={"#"} />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            alt="image contoh"
+            src={
+              "https://images.pexels.com/photos/32315717/pexels-photo-32315717.jpeg"
+            }
+            fill={true}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>desc</p>
+          <Button text={"See More"} url={"#"} />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            alt="image contoh"
+            src={
+              "https://images.pexels.com/photos/32315717/pexels-photo-32315717.jpeg"
+            }
+            fill={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 const Portfolio = () => {
-  return <div className={styles.container}>Portfolio</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a category</h1>
+      <div className={styles.items}>
+        <Link className={styles.item} href="/portfolio/websites">
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link className={styles.item} href="/portfolio/apps">
+          <span className={styles.title}>Applications</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
