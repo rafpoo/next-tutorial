72607124611736095507917633c716e492b58c5d
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2026-01-03 16:26:41 +0800

    feat: next auth implemented (ada satu error blm di fix)

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,6 +3,8 @@
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
   // const [data, setData] = useState([]);
@@ -26,16 +28,25 @@ const Dashboard = () => {
   //   };
   //   getData();
   // }, []);
-
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error, isLoading } = useSWR(
     `https://jsonplaceholder.typicode.com/posts`,
     fetcher
   );
+  const session = useSession();
+  const router = useRouter();
 
-  console.log(data);
+  if (session.status === "loading") {
+    return <p>Loading ...</p>;
+  }
 
-  return <div className={styles.container}>Dashboard</div>;
+  if (session.status === "unauthenticated") {
+    router?.push("/dashboard/login");
+  }
+
+  if (session.status === "authenticated") {
+    return <div className={styles.container}>Dashboard</div>;
+  }
 };
 
 export default Dashboard;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import React from "react";
 import styles from "./navbar.module.css";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
+import { signOut, useSession } from "next-auth/react";
 
 const links = [
   {
@@ -39,6 +40,7 @@ const links = [
 ];
 
 const Navbar = () => {
+  const session = useSession();
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
@@ -51,14 +53,12 @@ const Navbar = () => {
             {link.title}
           </Link>
         ))}
-        <button
-          className={styles.logout}
-          onClick={() => {
-            console.log("Logged out");
-          }}
-        >
-          Logout
-        </button>
+
+        {session.status === "authenticated" && (
+          <button className={styles.logout} onClick={signOut}>
+            Logout
+          </button>
+        )}
       </div>
     </div>
   );
